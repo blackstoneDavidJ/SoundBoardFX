6606132335db8b9bd1b050b7b84397f2d088b22b
David <dblackstone107@gmail.com>
2023-09-07 19:59:29 -0400

    testing listview

--- src/main/java/com/example/soundboardfx/HelloController.java
+++ src/main/java/com/example/soundboardfx/HelloController.java
@@ -1,18 +1,34 @@
 package com.example.soundboardfx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
+    public Button submitSoundPackButton;
     @FXML
     private Label welcomeText;
+    @FXML
+    private ListView<String> soundListView;
+    private ObservableList<String> soundList;
 
+    public HelloController() {
+        // Initialize the soundList
+        soundList = FXCollections.observableArrayList();
+        soundList.addAll();
+    }
     @FXML
-    private ListView<String> SoundListView;
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void submitNewSoundPack() {
+        soundList.add(String.valueOf(soundList.size() + 1));
+        soundListView.setItems(soundList); // Update the ListView
     }
 
+    public void onSoundClicked(MouseEvent mouseEvent)
+    {
+        System.out.println("Clicked on " + soundListView.getSelectionModel().getSelectedItem());
+    }
 }
